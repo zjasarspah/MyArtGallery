8e559f17075ddb6f86ff6d821083d7e4922ad113
Zerina <zjasarspah1@etf.unsa.ba>
2022-12-07 20:58:36 +0100

    Dao interface for ArtWork domain bean

---
+++ src/main/java/ba/unsa/etf/rpr/dao/ArtWorkDao.java
@@ -0,0 +1,31 @@
+package ba.unsa.etf.rpr.dao;
+
+import ba.unsa.etf.rpr.domain.ArtStyle;
+import ba.unsa.etf.rpr.domain.ArtWork;
+import ba.unsa.etf.rpr.domain.Artist;
+
+import java.util.List;
+
+/**
+ * Dao interface for ArtWork domain bean
+ * @author Zerina Jasarspahic
+ */
+
+public interface ArtWorkDao extends Dao<ArtWork> {
+
+    /**
+     * Returns all artworks that belong to given art style
+     * @param movement search movement
+     * @return list of artworks
+     */
+
+    List<ArtWork> searchByArtStyle(ArtStyle movement);
+
+    /**
+     * Returns all artworks that belong to given artist
+     * @param artist search artist
+     * @return list of artworks
+     */
+
+    List<ArtWork> searchByArtist(Artist artist);
+}
