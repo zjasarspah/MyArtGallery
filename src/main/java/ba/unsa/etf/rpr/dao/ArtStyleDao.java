127136330edc9fdb32db6170c93220b2b055e03a
Zerina <zjasarspah1@etf.unsa.ba>
2022-12-07 20:57:18 +0100

    Dao interface for ArtStyle domain bean

---
+++ src/main/java/ba/unsa/etf/rpr/dao/ArtStyleDao.java
@@ -0,0 +1,12 @@
+package ba.unsa.etf.rpr.dao;
+
+import ba.unsa.etf.rpr.domain.ArtStyle;
+
+/**
+ * Dao interface for ArtStyle domain bean
+ *
+ * @author Zerina Jasarspahic
+ */
+
+public interface ArtStyleDao extends Dao<ArtStyle> {
+}
