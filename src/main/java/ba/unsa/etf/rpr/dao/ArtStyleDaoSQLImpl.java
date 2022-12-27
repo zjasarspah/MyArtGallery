4b2d9b83ddf704a6e66e9300642cf9b63031336c
Zerina <zjasarspah1@etf.unsa.ba>
2022-12-28 00:37:59 +0100

    Update of ArtStyleDaoSQLImpl

--- src/main/java/ba/unsa/etf/rpr/dao/ArtStyleDaoSQLImpl.java
+++ src/main/java/ba/unsa/etf/rpr/dao/ArtStyleDaoSQLImpl.java
@@ -11,9 +11,9 @@ public class ArtStyleDaoSQLImpl implements ArtStyleDao {
     private Connection connection;
 
     public ArtStyleDaoSQLImpl(){
-        try{
-            this.connection = DriverManager.getConnection();
-        }catch (Exception e){
+        try {
+            this.connection = DataBaseDao.getInstance();
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
@@ -29,14 +29,14 @@ public class ArtStyleDaoSQLImpl implements ArtStyleDao {
 
             ResultSet rs = stmt.executeQuery();
             if (rs.next()){ // result set is iterator.
-                ArtStyle artstyle = new ArtStyle();
-                artstyle.setId(rs.getInt("id"));
-                artstyle.setName(rs.getString("name"));
+                ArtStyle item = new ArtStyle();
+                item.setId(rs.getInt("id"));
+                item.setName(rs.getString("name"));
+                item.setYear(rs.getString("year"));
                 rs.close();
-                return artstyle;
+                return item;
             }
-
-        }catch (SQLException e){
+        } catch (SQLException e){
             e.printStackTrace(); // poor error handling
         }
 
@@ -47,13 +47,18 @@ public class ArtStyleDaoSQLImpl implements ArtStyleDao {
     @Override
     public ArtStyle add(ArtStyle item) {
 
-        String insert = "INSERT INTO ArtStyles(id, name) VALUES(?)";
+        String insert = "INSERT INTO ArtStyles(name, year) VALUES(?, ?)";
 
         try {
             PreparedStatement stmt = this.connection.prepareStatement(insert, Statement.RETURN_GENERATED_KEYS);
-            stmt.setInt(1, item.getId());
-            stmt.setString(2, item.getName());
+            stmt.setString(1, item.getName());
+            stmt.setString(2, item.getYear());
             stmt.executeUpdate();
+
+            ResultSet rs = stmt.getGeneratedKeys();
+            rs.next();
+            item.setId(rs.getInt(1));
+            return item;
         } catch (SQLException e) {
             e.printStackTrace();
         }
@@ -63,11 +68,13 @@ public class ArtStyleDaoSQLImpl implements ArtStyleDao {
 
     @Override
     public ArtStyle update(ArtStyle item) {
-        String insert = "UPDATE ArtStyles SET name = ? WHERE id = ?";
+        String insert = "UPDATE ArtStyles SET name = ?, year = ? WHERE id = ?";
+
         try{
-            PreparedStatement stmt = this.connection.prepareStatement(insert, Statement.RETURN_GENERATED_KEYS);
+            PreparedStatement stmt = this.connection.prepareStatement(insert);
             stmt.setObject(1, item.getName());
-            stmt.setObject(2, item.getId());
+            stmt.setObject(2, item.getYear());
+            stmt.setObject(3, item.getId());
             stmt.executeUpdate();
         }catch (SQLException e){
             e.printStackTrace();
@@ -80,7 +87,7 @@ public class ArtStyleDaoSQLImpl implements ArtStyleDao {
     public void delete(int id) {
         String insert = "DELETE FROM ArtStyles WHERE id = ?";
         try{
-            PreparedStatement stmt = this.connection.prepareStatement(insert, Statement.RETURN_GENERATED_KEYS);
+            PreparedStatement stmt = this.connection.prepareStatement(insert);
             stmt.setObject(1, id);
             stmt.executeUpdate();
         }catch (SQLException e){
@@ -92,6 +99,7 @@ public class ArtStyleDaoSQLImpl implements ArtStyleDao {
     public List<ArtStyle> getAll() {
         String query = "SELECT * FROM ArtStyles";
         List<ArtStyle> artStyles = new ArrayList<>();
+
         try {
             PreparedStatement stmt = this.connection.prepareStatement(query);
             ResultSet rs = stmt.executeQuery();
@@ -99,6 +107,7 @@ public class ArtStyleDaoSQLImpl implements ArtStyleDao {
                 ArtStyle item = new ArtStyle();
                 item.setId(rs.getInt("id"));
                 item.setName(rs.getString("name"));
+                item.setYear(rs.getString("year"));
                 artStyles.add(item);
             }
             rs.close();
