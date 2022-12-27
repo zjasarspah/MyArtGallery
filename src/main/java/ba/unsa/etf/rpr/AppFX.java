8717f82a767b1e32a4037a2bafa8292b37e99675
Zerina <zjasarspah1@etf.unsa.ba>
2022-12-28 00:39:35 +0100

    Implementation of AppFX

---
+++ src/main/java/ba/unsa/etf/rpr/AppFX.java
@@ -0,0 +1,25 @@
+package ba.unsa.etf.rpr;
+
+import ba.unsa.etf.rpr.controllers.HomeController;
+import ba.unsa.etf.rpr.controllers.LoginController;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import static javafx.scene.control.PopupControl.USE_COMPUTED_SIZE;
+
+public class AppFX extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/fxml/login.fxml"));
+        LoginController loginController = new LoginController();
+        fxmlLoader.setController(loginController);
+        Parent root = fxmlLoader.load();
+        stage.setScene(new Scene(root, USE_COMPUTED_SIZE, USE_COMPUTED_SIZE));
+        stage.setTitle("My Art Gallery");
+        stage.setResizable(false);
+        stage.show();
+    }
+}
