568ff83e2ad9342aefe41195c038eb2e7278bde1
Zerina <zjasarspah1@etf.unsa.ba>
2022-12-28 01:06:36 +0100

    Refactoring of Dao, ArtWorkDao, ArtistDao

--- src/main/java/ba/unsa/etf/rpr/dao/ArtWorkDao.java
+++ src/main/java/ba/unsa/etf/rpr/dao/ArtWorkDao.java
@@ -3,6 +3,7 @@ package ba.unsa.etf.rpr.dao;
 import ba.unsa.etf.rpr.domain.ArtStyle;
 import ba.unsa.etf.rpr.domain.ArtWork;
 import ba.unsa.etf.rpr.domain.Artist;
+import ba.unsa.etf.rpr.exceptions.ArtGalleryException;
 
 import java.util.List;
 
@@ -19,7 +20,7 @@ public interface ArtWorkDao extends Dao<ArtWork> {
      * @return list of artworks
      */
 
-    List<ArtWork> searchByArtStyle(ArtStyle movement);
+    List<ArtWork> searchByArtStyle(ArtStyle movement) throws ArtGalleryException;
 
     /**
      * Returns all artworks that belong to given artist
@@ -27,5 +28,5 @@ public interface ArtWorkDao extends Dao<ArtWork> {
      * @return list of artworks
      */
 
-    List<ArtWork> searchByArtist(Artist artist);
+    List<ArtWork> searchByArtist(Artist artist) throws ArtGalleryException;
 }
