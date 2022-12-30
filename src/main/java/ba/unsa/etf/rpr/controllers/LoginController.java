3360f3265bf613ad672d3bbd7b5b2b362b479d6f
Zerina <zjasarspah1@etf.unsa.ba>
2022-12-30 01:12:15 +0100

    Refactoring of LoginController

--- src/main/java/ba/unsa/etf/rpr/controllers/LoginController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/LoginController.java
@@ -6,6 +6,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 import java.io.IOException;
 import static javafx.scene.control.PopupControl.USE_COMPUTED_SIZE;
@@ -13,10 +14,8 @@ import static javafx.scene.control.PopupControl.USE_COMPUTED_SIZE;
 public class LoginController {
 
     public Button btnLogin;
-
-    public LoginController() {
-    }
-
+    @FXML
+    public GridPane loginGridPane;
     @FXML
     void initialize() {
     }
@@ -27,12 +26,16 @@ public class LoginController {
             Parent root = fxmlLoader.load();
             Stage stage = new Stage();
             stage.setScene(new Scene(root, USE_COMPUTED_SIZE, USE_COMPUTED_SIZE));
-            stage.setResizable(false);
+            stage.setResizable(true);
+            stage.setTitle("My Art Gallery");
             stage.show();
+            ((Stage)loginGridPane.getScene().getWindow()).hide();
+            stage.setOnHiding(event -> {
+                ((Stage)loginGridPane.getScene().getWindow()).show();
+            });
         } catch (IOException e) {
             System.out.println("Something went wrong with opening new ArtistSearch window!");
             throw new RuntimeException(e);
         }
-
     }
 }
