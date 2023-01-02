7af0a9c798a9a6ee57e61526cad1228482aacf6f
Zerina <zjasarspah1@etf.unsa.ba>
2023-01-02 19:51:30 +0100

    Implementation of Controller class

---
+++ src/main/java/ba/unsa/etf/rpr/controllers/Controller.java
@@ -0,0 +1,23 @@
+package ba.unsa.etf.rpr.controllers;
+
+/***
+ * Abstract class for controllers
+ * From this class are extended all controller classes except MainController and LoginController
+ * @author Zerina Jasarspahic
+ */
+
+public abstract class Controller {
+
+    private Integer id;
+    public Controller (Integer id) {
+        this.id = id;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+}
