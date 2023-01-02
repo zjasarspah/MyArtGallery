59d05fe11affe18f99ee05f45a3bc3c76471053b
Zerina <zjasarspah1@etf.unsa.ba>
2023-01-02 19:34:19 +0100

    Refactoring of ArtsistManager

--- src/main/java/ba/unsa/etf/rpr/business/ArtistManager.java
+++ src/main/java/ba/unsa/etf/rpr/business/ArtistManager.java
@@ -3,23 +3,36 @@ package ba.unsa.etf.rpr.business;
 import ba.unsa.etf.rpr.dao.DaoFactory;
 import ba.unsa.etf.rpr.domain.Artist;
 import ba.unsa.etf.rpr.exceptions.ArtGalleryException;
+import java.util.Arrays;
 import java.util.List;
 
 /**
  * Business Logic Layer for Artist
  * @author Zerina Jasarspahic
  */
-
-//ova klasa se napravila radi citljivosti jer ako u kodu za kontroler napisemo (new DaoFactory).artistDao().getAll(); komplikuje se kod
-    //također ovo je napravljeno
 public class ArtistManager implements Manager<Artist> {
+
+    public void validateArtist(Artist a) throws ArtGalleryException{
+        if (a.getName() == null || a.getName().length() > 45 || a.getName().length() < 3){
+            throw new ArtGalleryException("Artist's name must be between 3 and 45 chars long.");
+        } else if (!isLifeSpanCorrect(a.getLifespan())) {
+            throw new ArtGalleryException("Lifespan of the artist isn't correct.");
+        }
+    }
 
     public List<Artist> getAll() throws ArtGalleryException {
         return DaoFactory.artistDao().getAll();
     }
 
     public void delete(int id) throws ArtGalleryException {
-        DaoFactory.artistDao().delete(id);
+        try{
+            DaoFactory.artistDao().delete(id);
+        } catch (ArtGalleryException e){
+            if (e.getMessage().contains("FOREIGN KEY")){
+                throw new ArtGalleryException("Can't delete artist which is related to artwork. First delete related artwork before deleting artist.");
+            }
+            throw e;
+        }
     }
 
     public Artist getById(int id) throws ArtGalleryException {
@@ -27,11 +40,41 @@ public class ArtistManager implements Manager<Artist> {
     }
 
     public void update(Artist artist) throws ArtGalleryException{
+        validateArtist(artist);
         DaoFactory.artistDao().update(artist);
     }
 
     public Artist add(Artist artist) throws ArtGalleryException{
+        validateArtist(artist);
+        (new ArtistManager()).isExist(artist);
         return DaoFactory.artistDao().add(artist);
     }
 
+    public List<Artist> searchByName(String name) throws ArtGalleryException {
+        return DaoFactory.artistDao().searchByName(name);
+    }
+
+    private void isExist(Artist artist) throws ArtGalleryException {
+        for (Artist a : getAll()) {
+            if (a.equals(artist)) {
+                throw new ArtGalleryException("This artist already exists.");
+            }
+        }
+    }
+
+    private boolean isLifeSpanCorrect (String lifeSpan) {
+        if (lifeSpan == null || lifeSpan.length() != 9 || lifeSpan.charAt(4) != '-') {
+            return false;
+        }
+
+        for (int i = 0; i < lifeSpan.length(); i++) {
+            if (i == 4) continue;
+            if (lifeSpan.charAt(i) < '0' || lifeSpan.charAt(i) > '9') {
+                return false;
+            }
+        }
+
+        List<String> years = Arrays.asList(lifeSpan.split("-"));
+        return years.get(0).compareTo(years.get(1)) < 0 && Integer.parseInt(years.get(1)) - Integer.parseInt(years.get(0)) <= 100;
+    }
 }
--- src/main/java/ba/unsa/etf/rpr/business/Manager.java
+++ src/main/java/ba/unsa/etf/rpr/business/Manager.java
@@ -1,20 +1,22 @@
 package ba.unsa.etf.rpr.business;
 
-import ba.unsa.etf.rpr.dao.DaoFactory;
-import ba.unsa.etf.rpr.domain.ArtWork;
-import ba.unsa.etf.rpr.domain.Idable;
 import ba.unsa.etf.rpr.exceptions.ArtGalleryException;
 
 import java.util.List;
 
+/**
+ * Interface for Business Logic Layer
+ * @author Zerina Jasarspahic
+ */
+
 public interface Manager <T> {
-    public List<T> getAll() throws ArtGalleryException;
+    List<T> getAll() throws ArtGalleryException;
 
-    public void delete(int id) throws ArtGalleryException;
+    void delete(int id) throws ArtGalleryException;
 
-    public T getById(int id) throws ArtGalleryException;
+    T getById(int id) throws ArtGalleryException;
 
-    public void update(T item) throws ArtGalleryException;
+    void update(T item) throws ArtGalleryException;
 
-    public T add(T item) throws ArtGalleryException;
+    T add(T item) throws ArtGalleryException;
 }
