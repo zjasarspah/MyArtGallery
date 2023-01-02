3461f373445a02745801adb2921697d58cb5f34d
Zerina <zjasarspah1@etf.unsa.ba>
2023-01-02 19:41:46 +0100

    Refactoring of Artist model

--- src/main/java/ba/unsa/etf/rpr/controllers/model/ArtistModel.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/model/ArtistModel.java
@@ -3,15 +3,29 @@ package ba.unsa.etf.rpr.controllers.model;
 import ba.unsa.etf.rpr.domain.Artist;
 import javafx.beans.property.SimpleStringProperty;
 
+/**
+ * Model for Artist
+ * @author Zerina Jasarspahic
+ */
+
 public class ArtistModel {
 
     public SimpleStringProperty name = new SimpleStringProperty("");
     public SimpleStringProperty  lifespan = new SimpleStringProperty("");
 
+    /**
+     * Public method for parsing Artist's fields into Properties
+     * @author Zerina Jasarspahic
+     */
     public void fromArtist(Artist a){
         this.name.set(a.getName());
         this.lifespan.set(a.getLifespan());
     }
+
+    /**
+     * Public method for parsing Properties into Artist's fields
+     * @author Zerina Jasarspahic
+     */
 
     public Artist toArtist(){
         Artist a = new Artist();
