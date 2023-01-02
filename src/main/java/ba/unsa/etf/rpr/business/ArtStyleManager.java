8b096bf31fd750daafb83cd6712aee7f1f197f79
Zerina <zjasarspah1@etf.unsa.ba>
2023-01-02 19:35:18 +0100

    Refactoring of Manager

--- src/main/java/ba/unsa/etf/rpr/business/ArtStyleManager.java
+++ src/main/java/ba/unsa/etf/rpr/business/ArtStyleManager.java
@@ -3,20 +3,37 @@ package ba.unsa.etf.rpr.business;
 import ba.unsa.etf.rpr.dao.DaoFactory;
 import ba.unsa.etf.rpr.domain.ArtStyle;
 import ba.unsa.etf.rpr.exceptions.ArtGalleryException;
+
+import java.util.Arrays;
 import java.util.List;
 
 /**
- * Business Logic Layer for Art style
+ * Business Logic Layer for Art Style
  * @author Zerina Jasarspahic
  */
 public class ArtStyleManager implements Manager<ArtStyle> {
+
+    public void validateArtStyle(ArtStyle as) throws ArtGalleryException{
+        if (as.getName() == null || as.getName().length() > 45 || as.getName().length() < 3){
+            throw new ArtGalleryException("Art Style's name must be between 3 and 45 chars long.");
+        } else if (!isDurationCorrect(as.getYear())) {
+            throw new ArtGalleryException("Duration of the art style isn't correct.");
+        }
+    }
 
     public List<ArtStyle> getAll() throws ArtGalleryException {
         return DaoFactory.artStyleDao().getAll();
     }
 
     public void delete(int id) throws ArtGalleryException {
-        DaoFactory.artStyleDao().delete(id);
+        try{
+            DaoFactory.artStyleDao().delete(id);
+        }catch (ArtGalleryException e){
+            if (e.getMessage().contains("FOREIGN KEY")){
+                throw new ArtGalleryException("Can't delete art style which is related to artwork. First delete related artwork before deleting artstyle.");
+            }
+            throw e;
+        }
     }
 
     public ArtStyle getById(int id) throws ArtGalleryException {
@@ -24,11 +41,43 @@ public class ArtStyleManager implements Manager<ArtStyle> {
     }
 
     public void update(ArtStyle artStyle) throws ArtGalleryException{
+        validateArtStyle(artStyle);
         DaoFactory.artStyleDao().update(artStyle);
     }
 
     public ArtStyle add(ArtStyle artStyle) throws ArtGalleryException{
+        validateArtStyle(artStyle);
+        (new ArtStyleManager()).isExist(artStyle);
         return DaoFactory.artStyleDao().add(artStyle);
+
+    }
+
+    public List<ArtStyle> searchByName (String name) throws ArtGalleryException {
+        return DaoFactory.artStyleDao().searchByName(name);
+    }
+
+    private void isExist(ArtStyle artStyle) throws ArtGalleryException {
+        for (ArtStyle as : getAll()) {
+            if (as.equals(artStyle)) {
+                throw new ArtGalleryException("This art style already exists.");
+            }
+        }
+    }
+
+    private boolean isDurationCorrect (String duration) {
+        if (duration == null || duration.length() != 9 || duration.charAt(4) != '-') {
+            return false;
+        }
+
+        for (int i = 0; i < duration.length(); i++) {
+            if (i == 4) continue;
+            if (duration.charAt(i) < '0' || duration.charAt(i) > '9') {
+                return false;
+            }
+        }
+
+        List<String> years = Arrays.asList(duration.split("-"));
+        return years.get(0).compareTo(years.get(1)) < 0;
     }
 
 }
--- src/main/java/ba/unsa/etf/rpr/business/ArtWorkManager.java
+++ src/main/java/ba/unsa/etf/rpr/business/ArtWorkManager.java
@@ -1,15 +1,31 @@
 package ba.unsa.etf.rpr.business;
 
 import ba.unsa.etf.rpr.dao.DaoFactory;
+import ba.unsa.etf.rpr.domain.ArtStyle;
 import ba.unsa.etf.rpr.domain.ArtWork;
+import ba.unsa.etf.rpr.domain.Artist;
 import ba.unsa.etf.rpr.exceptions.ArtGalleryException;
+
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
+import java.util.TreeSet;
 
 /**
  * Business Logic Layer for Artwork
  * @author Zerina Jasarspahic
  */
 public class ArtWorkManager implements Manager<ArtWork> {
+
+    public void validateArtWork(ArtWork aw) throws ArtGalleryException{
+        if (aw.getName() == null || aw.getName().length() > 45 || aw.getName().length() < 3){
+            throw new ArtGalleryException("Artwork's name must be between 3 and 45 chars long.");
+        } else if (aw.getArtist() == null) {
+            throw new ArtGalleryException("You forget to fill out information about Artist.");
+        } else if (aw.getMovement() == null) {
+            throw new ArtGalleryException("You forget to fill out information about Art Style.");
+        }
+    }
 
     public List<ArtWork> getAll() throws ArtGalleryException {
         return DaoFactory.artWorkDao().getAll();
@@ -24,11 +40,48 @@ public class ArtWorkManager implements Manager<ArtWork> {
     }
 
     public void update(ArtWork artWork) throws ArtGalleryException{
+        validateArtWork(artWork);
         DaoFactory.artWorkDao().update(artWork);
     }
 
     public ArtWork add(ArtWork artWork) throws ArtGalleryException{
+        validateArtWork(artWork);
+        (new ArtWorkManager()).isExist(artWork);
         return DaoFactory.artWorkDao().add(artWork);
     }
 
+    public List<ArtWork> searchByName (String name) throws ArtGalleryException {
+        return DaoFactory.artWorkDao().searchByName(name);
+    }
+
+    public List<ArtWork> searchByArtStyle(ArtStyle movement) throws ArtGalleryException {
+        return DaoFactory.artWorkDao().searchByArtStyle(movement);
+    }
+
+    public List<ArtWork> searchByArtist(Artist artist) throws ArtGalleryException {
+        return DaoFactory.artWorkDao().searchByArtist(artist);
+    }
+
+    public List<ArtWork> search(String item) throws ArtGalleryException {
+        TreeSet<ArtWork> set = new TreeSet<>(Comparator.comparing(ArtWork::getId));
+        set.addAll((new ArtWorkManager()).searchByName(item));
+        List<Artist> a = (new ArtistManager()).searchByName(item);
+        for (Artist artist : a) {
+            set.addAll((new ArtWorkManager()).searchByArtist(artist));
+        }
+        List<ArtStyle> as = (new ArtStyleManager()).searchByName(item);
+        for (ArtStyle style : as) {
+            set.addAll((new ArtWorkManager()).searchByArtStyle(style));
+        }
+
+        return new ArrayList<>(set);
+    }
+
+    private void isExist(ArtWork artwork) throws ArtGalleryException {
+        for (ArtWork aw : getAll()) {
+            if (aw.equals(artwork)) {
+                throw new ArtGalleryException("This artwork already exists.");
+            }
+        }
+    }
 }
