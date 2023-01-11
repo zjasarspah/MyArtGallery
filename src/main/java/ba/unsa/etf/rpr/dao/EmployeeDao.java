e2156840b497edbc8449f280e2ba7bbaf4b73fe3
Zerina <zjasarspah1@etf.unsa.ba>
2023-01-11 16:06:43 +0100

    Implementation of EmployeeDao

---
+++ src/main/java/ba/unsa/etf/rpr/dao/EmployeeDao.java
@@ -0,0 +1,11 @@
+package ba.unsa.etf.rpr.dao;
+
+import ba.unsa.etf.rpr.domain.Employee;
+
+/**
+ * Dao interface for Employee domain bean
+ * @author Zerina Jasarspahic
+ */
+
+public interface EmployeeDao extends Dao<Employee>{
+}
