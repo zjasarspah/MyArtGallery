84c24665120e143d531180019db0b6986125d3df
Zerina <zjasarspah1@etf.unsa.ba>
2023-01-11 14:41:44 +0100

    Refactoring of ArtistDao

--- src/main/java/ba/unsa/etf/rpr/dao/ArtistDao.java
+++ src/main/java/ba/unsa/etf/rpr/dao/ArtistDao.java
@@ -1,10 +1,6 @@
 package ba.unsa.etf.rpr.dao;
 
-import ba.unsa.etf.rpr.domain.ArtStyle;
 import ba.unsa.etf.rpr.domain.Artist;
-import ba.unsa.etf.rpr.exceptions.ArtGalleryException;
-
-import java.util.List;
 
 /**
  * Dao interface for Artist domain bean
