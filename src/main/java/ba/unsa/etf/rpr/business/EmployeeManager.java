c84b721812e5c1c1e0202de4bd33888633b8ff23
Zerina <zjasarspah1@etf.unsa.ba>
2023-01-11 16:07:21 +0100

    Implementation of EmployeeManager

---
+++ src/main/java/ba/unsa/etf/rpr/business/EmployeeManager.java
@@ -0,0 +1,27 @@
+package ba.unsa.etf.rpr.business;
+
+import ba.unsa.etf.rpr.dao.DaoFactory;
+import ba.unsa.etf.rpr.domain.Employee;
+import ba.unsa.etf.rpr.exceptions.ArtGalleryException;
+import java.util.List;
+
+/**
+ * Business Logic Layer for Employee
+ * @author Zerina Jasarspahic
+ */
+public class EmployeeManager {
+
+    public List<Employee> getAll() throws ArtGalleryException {
+        return DaoFactory.employeeDao().getAll();
+    }
+    public boolean isValid (Employee employee) throws ArtGalleryException {
+        for (Employee e : getAll()) {
+            if (e.equals(employee)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+}
