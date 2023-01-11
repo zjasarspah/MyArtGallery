092a2214b75e002443cff9605b1deb247b1bd496
Zerina <zjasarspah1@etf.unsa.ba>
2023-01-11 14:56:20 +0100

    Refactoring of ArtWorkDaoSQLImpl

--- src/main/java/ba/unsa/etf/rpr/dao/ArtWorkDaoSQLImpl.java
+++ src/main/java/ba/unsa/etf/rpr/dao/ArtWorkDaoSQLImpl.java
@@ -4,12 +4,15 @@ import ba.unsa.etf.rpr.domain.ArtStyle;
 import ba.unsa.etf.rpr.domain.ArtWork;
 import ba.unsa.etf.rpr.domain.Artist;
 import ba.unsa.etf.rpr.exceptions.ArtGalleryException;
-
 import java.sql.*;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
+
+/**
+ * MySQL Implementation of ArtWorkDao
+ * @author Zerina Jasarspahic
+ */
 
 public class ArtWorkDaoSQLImpl extends AbstractDao<ArtWork> implements ArtWorkDao {
 
@@ -26,7 +29,7 @@ public class ArtWorkDaoSQLImpl extends AbstractDao<ArtWork> implements ArtWorkDa
             q.setId(rs.getInt("id"));
             q.setName(rs.getString("name"));
             q.setArtist(DaoFactory.artistDao().getById(rs.getInt("id_artist")));
-            q.setMovement(DaoFactory.artStyleDao().getById(rs.getInt("id_artstyle")));
+            q.setArtStyle(DaoFactory.artStyleDao().getById(rs.getInt("id_artstyle")));
             return q;
         } catch (Exception e) {
             throw new ArtGalleryException(e.getMessage(), e);
@@ -39,18 +42,17 @@ public class ArtWorkDaoSQLImpl extends AbstractDao<ArtWork> implements ArtWorkDa
         row.put("id", object.getId());
         row.put("name", object.getName());
         row.put("id_artist", object.getArtist().getId());
-        row.put("id_artstyle", object.getMovement().getId());
+        row.put("id_artstyle", object.getArtStyle().getId());
         return row;
     }
 
     @Override
-    public List<ArtWork> searchByArtStyle(ArtStyle movement) throws ArtGalleryException {
-        return executeQuery("SELECT * FROM Artworks WHERE id_artstyle = ?", new Object[]{movement.getId()});
+    public List<ArtWork> searchByArtStyle(ArtStyle artStyle) throws ArtGalleryException {
+        return executeQuery("SELECT * FROM Artworks WHERE id_artstyle = ?", new Object[]{artStyle.getId()});
     }
 
     @Override
     public List<ArtWork> searchByArtist(Artist artist) throws  ArtGalleryException{
         return executeQuery("SELECT * FROM Artworks WHERE id_artist = ?", new Object[]{artist.getId()});
     }
-
 }
