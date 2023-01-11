fefc03d8a02af9106c3004de185c844f7f145f60
Zerina <zjasarspah1@etf.unsa.ba>
2023-01-11 14:09:15 +0100

    Refactoring of AppFX

--- src/main/java/ba/unsa/etf/rpr/AppFX.java
+++ src/main/java/ba/unsa/etf/rpr/AppFX.java
@@ -1,21 +1,20 @@
 package ba.unsa.etf.rpr;
 
-import ba.unsa.etf.rpr.controllers.MainController;
-import ba.unsa.etf.rpr.controllers.LoginController;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-
 import static javafx.scene.control.PopupControl.USE_COMPUTED_SIZE;
 
+/**
+ * Main class for working with JavaFX framework
+ * @author Zerina Jasarspahic
+ */
 public class AppFX extends Application {
     @Override
     public void start(Stage stage) throws Exception {
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/fxml/artgallerylogin.fxml"));
-        LoginController loginController = new LoginController();
-        //fxmlLoader.setController(loginController);
         Parent root = fxmlLoader.load();
         stage.setScene(new Scene(root, USE_COMPUTED_SIZE, USE_COMPUTED_SIZE));
         stage.setTitle("My Art Gallery");
