d8190ead1ac347610c9ee5874936307efb043ab3
Zerina <zjasarspah1@etf.unsa.ba>
2023-04-01 01:09:21 +0200

    Implementation of SingleButtonCellFactory

---
+++ src/main/java/ba/unsa/etf/rpr/controllers/components/SingleButtonCellFactory.java
@@ -0,0 +1,29 @@
+package ba.unsa.etf.rpr.controllers.components;
+
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.control.TableCell;
+import javafx.scene.control.TableColumn;
+import javafx.util.Callback;
+
+/**
+ * Triple button cell factory for creation of buttons for each cell in the table
+ * @author Zerina Jasarspahic
+ * @param <T>
+ */
+public class SingleButtonCellFactory<T> implements Callback<TableColumn<T, T>, TableCell<T, T>> {
+
+    private final EventHandler<ActionEvent> button;
+
+    /**
+     * @param button - event handler for button View
+     */
+    public SingleButtonCellFactory(EventHandler<ActionEvent> button) {
+        this.button = button;
+    }
+
+    @Override
+    public TableCell<T, T> call(TableColumn<T, T> objectTableColumn) {
+        return new SingleButtonTableCell<>(button);
+    }
+}
