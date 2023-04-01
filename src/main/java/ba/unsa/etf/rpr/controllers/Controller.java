6bd3bcc59e92413eef5817108dec3e5de349f946
Zerina <zjasarspah1@etf.unsa.ba>
2023-04-01 18:13:00 +0200

    Refactoring of Controller

--- src/main/java/ba/unsa/etf/rpr/controllers/Controller.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/Controller.java
@@ -2,12 +2,13 @@ package ba.unsa.etf.rpr.controllers;
 
 /***
  * Abstract class for controllers
- * From this class are extended all controller classes except MainController and LoginController
+ * From this class are extended all controller classes except LoginController, MainEmployeeController and MainGuestController
  * @author Zerina Jasarspahic
  */
 
 public abstract class Controller {
     private Integer id;
+    public Controller() {}
     public Controller (Integer id) {
         this.id = id;
     }
