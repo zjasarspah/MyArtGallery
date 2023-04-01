89430519758e144c69fbfd722fa66a1a15c0c5c3
Zerina <zjasarspah1@etf.unsa.ba>
2023-04-01 18:15:38 +0200

    Refactoring of ArtWorkManager

--- src/main/java/ba/unsa/etf/rpr/business/ArtWorkManager.java
+++ src/main/java/ba/unsa/etf/rpr/business/ArtWorkManager.java
@@ -44,15 +44,33 @@ public class ArtWorkManager implements Manager<ArtWork> {
         return DaoFactory.artWorkDao().searchByName(name);
     }
 
-    private List<ArtWork> searchByArtStyle(ArtStyle movement) throws ArtGalleryException {
-        return DaoFactory.artWorkDao().searchByArtStyle(movement);
+    /**
+     * Method that returns the list of artworks from the database that have the same art style
+     * @param artStyle we search
+     * @throws ArtGalleryException if art style doesn't exist in the database
+     */
+    private List<ArtWork> searchByArtStyle(ArtStyle artStyle) throws ArtGalleryException {
+        return DaoFactory.artWorkDao().searchByArtStyle(artStyle);
     }
 
+    /**
+     * Method that returns the list of artworks from the database that have the same artist
+     * @param artist we search
+     * @throws ArtGalleryException if artist doesn't exist in the database
+     */
     private List<ArtWork> searchByArtist(Artist artist) throws ArtGalleryException {
         return DaoFactory.artWorkDao().searchByArtist(artist);
     }
 
+    /**
+     * Method that returns the list of artworks from the database that have the same name, artist or art style as input item
+     * @param item we search
+     * @throws ArtGalleryException if item wasn't found in the database
+     */
     public List<ArtWork> search(String item) throws ArtGalleryException {
+        /* TreeSet is used because we don't want duplication in searching */
+        /* Elements in TreeSet are sorted */
+        /* Because we have a TreeSet of art works (type ArtWork), it is necessary to define a criterion by which the elements in the TreeSet are compared (in this case it is field id)*/
         TreeSet<ArtWork> set = new TreeSet<>(Comparator.comparing(ArtWork::getId));
         set.addAll((new ArtWorkManager()).searchByName(item));
         List<Artist> a = (new ArtistManager()).searchByName(item);
@@ -67,16 +85,26 @@ public class ArtWorkManager implements Manager<ArtWork> {
         return new ArrayList<>(set);
     }
 
-    public void validateArtWork(ArtWork aw) throws ArtGalleryException{
-        if (aw.getName() == null || aw.getName().length() > 45 || aw.getName().length() < 3){
+    /**
+     * Method for checking are the information about the artwork correct
+     * @param artwork for checking
+     * @throws ArtGalleryException if information about the artwork are not correct
+     */
+    public void validateArtWork(ArtWork artwork) throws ArtGalleryException{
+        if (artwork.getName() == null || artwork.getName().length() > 45 || artwork.getName().length() < 3){
             throw new ArtGalleryException("Artwork's name must be between 3 and 45 chars long.");
-        } else if (aw.getArtist() == null) {
+        } else if (artwork.getArtist() == null) {
             throw new ArtGalleryException("You forget to fill out information about Artist.");
-        } else if (aw.getArtStyle() == null) {
+        } else if (artwork.getArtStyle() == null) {
             throw new ArtGalleryException("You forget to fill out information about Art Style.");
         }
     }
 
+    /**
+     * Method for checking does the artwork already exist in the database
+     * @param artwork we search
+     * @throws ArtGalleryException if the artwork already exists in the database
+     */
     private void isExist(ArtWork artwork) throws ArtGalleryException {
         for (ArtWork aw : getAll()) {
             if (aw.equals(artwork)) {
