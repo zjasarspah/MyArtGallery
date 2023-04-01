ef8a18de9630bcbb5ba683fcb20a75806f044937
Zerina <zjasarspah1@etf.unsa.ba>
2023-04-01 18:15:21 +0200

    Refactoring of MainEmployeeController

--- src/main/java/ba/unsa/etf/rpr/controllers/MainEmployeeController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/MainEmployeeController.java
@@ -14,18 +14,15 @@ import ba.unsa.etf.rpr.exceptions.ArtGalleryException;
 import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.collections.ObservableList;
 import javafx.scene.control.Button;
 import javafx.scene.control.cell.PropertyValueFactory;
-import javafx.scene.layout.Region;
-import javafx.stage.Stage;
 import java.util.*;
 
 /**
- * Controller for main window
+ * Controller for main window if you are the employee
+ * Employees can view, add or edit the information about artworks, art styles and artists
  * @author Zerina Jasarspahic
  */
 public class MainEmployeeController {
@@ -83,117 +80,32 @@ public class MainEmployeeController {
 
     /**
      * Public method for button that adds the artist
+     * @param event on add button pressed
      */
     public void btnActionAddArtist(ActionEvent event){
-        editAddScene(new ArtistController(null), tabScreen, null,"/artist/addArtist.fxml", "Add Artist");
+        (new OpenNewWindow()).openDialog(new ArtistController(null), tabScreen,"/artist/addArtist.fxml", "Add Artist");
     }
 
     /**
      * Public method for button that adds the art style
+     * @param actionEvent on add button pressed
      */
     public void btnActionAddArtStyle(ActionEvent actionEvent) {
-        editAddScene(new ArtStyleController(null), tabScreen, null,"/artstyle/addArtStyle.fxml", "Add Art Style");
+        (new OpenNewWindow()).openDialog(new ArtStyleController(null), tabScreen,"/artstyle/addArtStyle.fxml", "Add Art Style");
     }
 
     /**
      * Public method for button that adds the artwork
+     * @param actionEvent on add button pressed
      */
     public void btnActionAddArtWork(ActionEvent actionEvent) {
-        editAddScene(new ArtworkController (null), tabScreen, null, "/artwork/addArtwork.fxml", "Add Artwork");
-    }
-
-    /**
-     * Public method for button that refreshes list of artworks
-     */
-    public void btnActionRefreshArtworks(ActionEvent actionEvent) throws ArtGalleryException {
-        artworks = FXCollections.observableArrayList(artWorkManager.getAll());
-        setColumns(colArtworkId, colArtworkName, actionColumnArtwork);
-        setButtonsInArtworkColumns (actionColumnArtwork, tabScreen, artWorkManager);
-        refresh(artworkTableView, artworks);
-    }
-
-    /**
-     * Public method for button that refreshes list of artists
-     */
-    public void btnActionRefreshArtists(ActionEvent actionEvent) throws ArtGalleryException {
-        artists = FXCollections.observableArrayList(artistManager.getAll());
-        setColumns(colArtistId, colArtistName, actionColumnArtist);
-        setButtonsInArtistColumns (actionColumnArtist, tabScreen, artistManager);
-        refresh(artistTableView, artists);
-    }
-
-    /**
-     * Public method for button that refresh list of art styles
-     */
-    public void btnActionRefreshArtStyles(ActionEvent actionEvent) throws ArtGalleryException {
-        artStyles = FXCollections.observableArrayList(artStyleManager.getAll());
-        setColumns(colArtStyleId, colArtStyleName, actionColumnArtStyle);
-        setButtonsInArtStyleColumns (actionColumnArtStyle, tabScreen, artStyleManager);
-        refresh(artStyleTableView, artStyles);
-    }
-
-    /**
-     * Private method for setting columns
-     */
-    private static void setColumns (TableColumn<Object, Object> columnId, TableColumn<Object, Object> columnName, TableColumn<Object, Object> actionColumn) {
-        columnId.setCellValueFactory(new PropertyValueFactory<>("id"));
-        columnName.setCellValueFactory(new PropertyValueFactory<>("name"));
-        actionColumn.setCellValueFactory(new PropertyValueFactory<>("id"));
-    }
-
-    /**
-     * Private method for setting buttons in columns for each art style
-     * View, Delete and Edit buttons
-     */
-    private static void setButtonsInArtStyleColumns (TableColumn<Object, Object> actionColumn, TabPane tabScreen, Manager manager) {
-        actionColumn.setCellFactory(new TripleButtonCellFactory(viewEvent -> {
-            int id = Integer.parseInt(((Button) viewEvent.getSource()).getUserData().toString());
-            editAddScene(new ViewArtStyleController(id), tabScreen, id, "/artstyle/viewArtStyle.fxml",  "View ArtStyle");
-        }, editEvent -> {
-            int id = Integer.parseInt(((Button) editEvent.getSource()).getUserData().toString());
-            editAddScene(new ArtStyleController(id),tabScreen, id, "/artstyle/addArtStyle.fxml", "Edit Art Style");
-        }, deleteEvent -> {
-            int id = Integer.parseInt(((Button) deleteEvent.getSource()).getUserData().toString());
-            delete(id, manager);
-        }));
-    }
-
-    /**
-     * Private method for setting buttons in columns for each artist
-     * View, Delete and Edit buttons
-     */
-    private static void setButtonsInArtistColumns (TableColumn<Object, Object> actionColumn, TabPane tabScreen, Manager manager) {
-        actionColumn.setCellFactory(new TripleButtonCellFactory(viewEvent -> {
-            int id = Integer.parseInt(((Button) viewEvent.getSource()).getUserData().toString());
-            editAddScene(new ViewArtistController(id), tabScreen, id, "/artist/viewArtist.fxml",  "View Artist");
-        }, editEvent -> {
-            int id = Integer.parseInt(((Button) editEvent.getSource()).getUserData().toString());
-            editAddScene(new ArtistController(id),tabScreen, id, "/artist/addArtist.fxml", "Edit Artist");
-        }, deleteEvent -> {
-            int id = Integer.parseInt(((Button) deleteEvent.getSource()).getUserData().toString());
-            delete(id, manager);
-        }));
-    }
-
-    /**
-     * Private method for setting buttons in columns for each artwork
-     * View, Delete and Edit buttons
-     */
-    private static void setButtonsInArtworkColumns (TableColumn<Object, Object> actionColumn, TabPane tabScreen, Manager manager) {
-        actionColumn.setCellFactory(new TripleButtonCellFactory(viewEvent -> {
-            int id = Integer.parseInt(((Button) viewEvent.getSource()).getUserData().toString());
-            editAddScene(new ViewArtworkController(id), tabScreen, id, "/artwork/viewArtwork.fxml",  "View Artwork");
-        }, editEvent -> {
-            int id = Integer.parseInt(((Button) editEvent.getSource()).getUserData().toString());
-            editAddScene(new ArtworkController(id),tabScreen, id, "/artwork/addArtwork.fxml", "Edit Artwork");
-        }, deleteEvent -> {
-            int id = Integer.parseInt(((Button) deleteEvent.getSource()).getUserData().toString());
-            delete(id, manager);
-        }));
+        (new OpenNewWindow()).openDialog(new ArtworkController (null), tabScreen, "/artwork/addArtwork.fxml", "Add Artwork");
     }
 
     /**
      * Private method for deleting artist, art style or artwork
+     * @param manager to check is it possible to delete artwork, artist or art style
+     * @param id to define which artist, artwork or art style should be deleted
      */
     private static void delete(Integer id, Manager manager){
         try {
@@ -208,24 +120,6 @@ public class MainEmployeeController {
     }
 
     /**
-     * Private method for editing or adding artist, art style or artwork
-     */
-    private static void editAddScene(Controller controller, TabPane tabScreen, Integer id, String window, String title) {
-        try{
-            tabScreen.getScene().getWindow().hide();
-            FXMLLoader loader = new FXMLLoader(MainEmployeeController.class.getResource(window));
-            loader.setController(controller);
-            Stage stage = new Stage();
-            stage.setScene(new Scene(loader.load(), Region.USE_COMPUTED_SIZE, Region.USE_COMPUTED_SIZE));
-            stage.setTitle(title);
-            stage.show();
-            stage.setOnHiding(event -> ((Stage)tabScreen.getScene().getWindow()).show());
-        }catch (Exception e){
-            new Alert(Alert.AlertType.NONE, e.getMessage(), ButtonType.OK).show();
-        }
-    }
-
-    /**
      * Private method for refreshing the window
      */
     static private void refresh(TableView<Object> tableView, ObservableList<Object> list){
@@ -234,35 +128,149 @@ public class MainEmployeeController {
     }
 
     /**
+     * Public method for refresh button
+     * @param list of artists, artworks or art styles
+     * @param pathView for the view window
+     * @param pathEdit for the edit window
+     * @param titleView for the view window
+     * @param titleEdit for the edit window
+     * @param tableView table with artists, artworks or art styles
+     * @param controllerView for view window
+     * @param controllerEdit for edit window
+     * @param columns in the table with id, name and button for each artist, artwork or art style
+     */
+
+    private void refreshButton (ObservableList<Object> list, TableView<Object> tableView, Controller controllerView, Controller controllerEdit, ArrayList<TableColumn<Object, Object>> columns, TabPane tabScreen, String pathView, String titleView, String pathEdit, String titleEdit, Manager manager){
+        setColumns(columns.get(0), columns.get(1), columns.get(2));
+        setButtonsInColumns (manager, controllerView, controllerEdit, columns.get(2), tabScreen, pathView, titleView, pathEdit, titleEdit);
+        refresh(tableView, list);
+    }
+
+    /**
+     * Public method for button that refreshes list of artworks
+     * @param actionEvent on refresh button pressed
+     */
+    public void btnActionRefreshArtworks(ActionEvent actionEvent) throws ArtGalleryException {
+        ArrayList<TableColumn<Object, Object>> columnArtWork = new ArrayList<TableColumn<Object, Object>>() {{
+            add(colArtworkId);
+            add(colArtworkName);
+            add(actionColumnArtwork);
+        }};
+        artworks = FXCollections.observableArrayList(artWorkManager.getAll());
+        refreshButton(artworks,artworkTableView,new ViewArtworkController(), new ArtworkController(),columnArtWork, tabScreen,"/artwork/viewArtwork.fxml",  "View Artwork", "/artwork/addArtwork.fxml", "Edit Artwork", artWorkManager);
+    }
+
+    /**
+     * Public method for button that refreshes list of artists
+     * @param actionEvent on refresh button pressed
+     */
+    public void btnActionRefreshArtists(ActionEvent actionEvent) throws ArtGalleryException {
+        ArrayList<TableColumn<Object, Object>> columnArtist = new ArrayList<TableColumn<Object, Object>>() {{
+            add(colArtistId);
+            add(colArtistName);
+            add(actionColumnArtist);
+        }};
+        artists = FXCollections.observableArrayList(artistManager.getAll());
+        refreshButton(artists,artistTableView,new ViewArtistController(), new ArtistController(), columnArtist, tabScreen, "/artist/viewArtist.fxml", "View Artist", "/artist/addArtist.fxml", "Edit Artist", artistManager);
+    }
+
+    /**
+     * Public method for button that refresh list of art styles
+     * @param actionEvent on refresh button pressed
+     */
+    public void btnActionRefreshArtStyles(ActionEvent actionEvent) throws ArtGalleryException {
+        ArrayList<TableColumn<Object, Object>> columnArtStyle = new ArrayList<TableColumn<Object, Object>>() {{
+            add(colArtStyleId);
+            add(colArtStyleName);
+            add(actionColumnArtStyle);
+        }};
+
+        artStyles = FXCollections.observableArrayList(artStyleManager.getAll());
+        refreshButton(artStyles,artStyleTableView,new ViewArtStyleController(), new ArtStyleController(), columnArtStyle, tabScreen,"/artstyle/viewArtStyle.fxml",  "View Art Style", "/artstyle/addArtStyle.fxml", "Edit Art Style", artStyleManager);
+    }
+
+
+    /**
+     * Private method for setting columns
+     * @param columnId id
+     * @param columnName name
+     * @param actionColumn buttons
+     */
+    private static void setColumns (TableColumn<Object, Object> columnId, TableColumn<Object, Object> columnName, TableColumn<Object, Object> actionColumn) {
+        columnId.setCellValueFactory(new PropertyValueFactory<>("id"));
+        columnName.setCellValueFactory(new PropertyValueFactory<>("name"));
+        actionColumn.setCellValueFactory(new PropertyValueFactory<>("id"));
+    }
+
+    /**
      * Public method for text field that searches artwork by name, artist or art style
+     * @param event enter pressed for searching
      */
     public void txtFieldActionSearchArtworks(ActionEvent event) throws ArtGalleryException {
+        ArrayList<TableColumn<Object, Object>> columnArtWork = new ArrayList<TableColumn<Object, Object>>() {{
+            add(colArtworkId);
+            add(colArtworkName);
+            add(actionColumnArtwork);
+        }};
         String item = txtFieldSearchArtworks.getText();
         artworks = FXCollections.observableArrayList(artWorkManager.search(item));
-        setColumns(colArtworkId,colArtworkName,actionColumnArtwork);
-        setButtonsInArtStyleColumns (actionColumnArtwork, tabScreen, artWorkManager);
-        refresh(artworkTableView, artworks);
+        refreshButton(artworks,artworkTableView,new ViewArtworkController(), new ArtworkController(),columnArtWork, tabScreen,"/artwork/viewArtwork.fxml",  "View Artwork", "/artwork/addArtwork.fxml", "Edit Artwork", artWorkManager);
     }
 
     /**
      * Public method for text field that searches artist by name
+     * @param event enter pressed for searching
      */
     public void txtFieldActionSearchArtist (ActionEvent event) throws ArtGalleryException {
+        ArrayList<TableColumn<Object, Object>> columnArtist = new ArrayList<TableColumn<Object, Object>>() {{
+            add(colArtistId);
+            add(colArtistName);
+            add(actionColumnArtist);
+        }};
         String search = txtFieldSearchArtist.getText();
         artists = FXCollections.observableArrayList(artistManager.searchByName(search));
-        setColumns(colArtistId, colArtistName, actionColumnArtist);
-        setButtonsInArtistColumns (actionColumnArtist, tabScreen, artistManager);
-        refresh(artistTableView, artists);
+        refreshButton(artists,artistTableView,new ViewArtistController(), new ArtistController(), columnArtist, tabScreen, "/artist/viewArtist.fxml", "View Artist", "/artist/addArtist.fxml", "Edit Artist", artistManager);
     }
 
     /**
      * Public method for text field that searches art style by name
+     * @param event enter pressed for searching
      */
     public void txtFieldActionSearchArtStyle (ActionEvent event) throws ArtGalleryException {
+        ArrayList<TableColumn<Object, Object>> columnArtStyle = new ArrayList<TableColumn<Object, Object>>() {{
+            add(colArtStyleId);
+            add(colArtStyleName);
+            add(actionColumnArtStyle);
+        }};
         String search = txtFieldSearchArtStyle.getText();
         artStyles = FXCollections.observableArrayList(artStyleManager.searchByName(search));
-        setColumns(colArtStyleId, colArtStyleName, actionColumnArtStyle);
-        setButtonsInArtStyleColumns (actionColumnArtStyle, tabScreen, artStyleManager);
-        refresh(artStyleTableView, artStyles);
+        refreshButton(artStyles,artStyleTableView,new ViewArtStyleController(), new ArtStyleController(), columnArtStyle, tabScreen,"/artstyle/viewArtStyle.fxml",  "View ArtStyle", "/artstyle/addArtStyle.fxml", "Edit ArtStyle", artStyleManager);
+    }
+
+    /**
+     * Private method for setting buttons in columns
+     * View, Edit and Delete button
+     * @param controllerView for view window
+     * @param controllerEdit for edit window
+     * @param actionColumn in which column we want to set buttons
+     * @param pathView for view window
+     * @param titleView for view window
+     * @param pathEdit for edit window
+     * @param titleEdit for edit window
+     * @param manager for checking is it possible to do editing
+     */
+    private static void setButtonsInColumns (Manager manager, Controller controllerView, Controller controllerEdit, TableColumn<Object, Object> actionColumn, TabPane tabScreen, String pathView, String titleView, String pathEdit, String titleEdit) {
+        actionColumn.setCellFactory(new TripleButtonCellFactory<>(viewEvent -> {
+            int id = Integer.parseInt(((Button) viewEvent.getSource()).getUserData().toString());
+            controllerView.setId(id);
+            (new OpenNewWindow()).openDialog(controllerView, tabScreen,  pathView,  titleView);
+        }, editEvent -> {
+            int id = Integer.parseInt(((Button) editEvent.getSource()).getUserData().toString());
+            controllerEdit.setId(id);
+            (new OpenNewWindow()).openDialog(controllerEdit,tabScreen, pathEdit, titleEdit);
+        }, deleteEvent -> {
+            int id = Integer.parseInt(((Button) deleteEvent.getSource()).getUserData().toString());
+            delete(id, manager);
+        }));
     }
 }
