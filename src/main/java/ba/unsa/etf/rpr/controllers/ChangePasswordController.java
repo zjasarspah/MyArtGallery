2d44fc8fed667e42f82577f39364b88abea06ee0
Zerina <zjasarspah1@etf.unsa.ba>
2023-04-01 18:15:50 +0200

    Refactoring of ChangePasswordController

--- src/main/java/ba/unsa/etf/rpr/controllers/ChangePasswordController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/ChangePasswordController.java
@@ -32,7 +32,13 @@ public class ChangePasswordController {
     @FXML
     public TextField txtFieldVerifyPassword;
 
-
+    /**
+     * Method that verifies the given username and password of the employee
+     * For changing the password, the employee must be in the art gallery data base
+     * For changing password, tne new password and the verified password must be the same
+     * @param actionEvent save button presses
+     * @throws Exception
+     */
     public void btnActionSave(ActionEvent actionEvent){
         try {
             Employee e = new Employee();
@@ -40,12 +46,16 @@ public class ChangePasswordController {
             String verifyPassword = txtFieldVerifyPassword.getText();
             e.setUsername(txtFieldUsername.getText());
             e.setPassword(txtFieldCurrentPassword.getText());
-            (new EmployeeManager()).update(e, newPassword, verifyPassword);
+            (new EmployeeManager()).verifyPassword(e, newPassword, verifyPassword);
             changePasswordGridPane.getScene().getWindow().hide();
         } catch (Exception e) {
             new Alert(Alert.AlertType.NONE, e.getMessage(), ButtonType.OK).show();
         }
     }
 
+    /**
+     * Method that exits the current window without saving any changes to the database
+     * @param actionEvent exit button pressed
+     */
     public void btnActionCancel(ActionEvent actionEvent){changePasswordGridPane.getScene().getWindow().hide();}
 }
